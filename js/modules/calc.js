007677cc09c006536327c70392bb1c1dca05d489
AVN <avnesterov88@gmail.com>
2021-12-17 15:15:41 +0700

    r4.72
    modules and services, bugfix

--- js/modules/calc.js
+++ js/modules/calc.js
@@ -1,5 +1,4 @@
 function calc() {
-    // Calculator
 
     const result = document.querySelector('.calculating__result span');
     let sex , height, weight, age, ratio;
@@ -109,4 +108,4 @@ function calc() {
     getDynamicInformation('#age');
 }
 
-module.exports = calc;
+export default calc;
